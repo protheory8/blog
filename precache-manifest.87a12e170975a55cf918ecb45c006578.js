b9a29735b6185a165e38fea0eca4fd1cdbfd351f
protheory8 <editaw5@gmail.com>
2020-08-17 17:33:57 +0500

    Updates

---
+++ precache-manifest.87a12e170975a55cf918ecb45c006578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7705664497d50b1c55d21d739b1be18a",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "ebc52034c08c9bbb7376",
+    "url": "/blog/static/css/main.d5faa8f2.chunk.css"
+  },
+  {
+    "revision": "6c07ef56d7060514789d",
+    "url": "/blog/static/js/2.8dfd4d94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog/static/js/2.8dfd4d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc52034c08c9bbb7376",
+    "url": "/blog/static/js/main.b364b94f.chunk.js"
+  },
+  {
+    "revision": "9b54a20457ee692fd5e6",
+    "url": "/blog/static/js/runtime-main.50ab1eed.js"
+  }
+]);
